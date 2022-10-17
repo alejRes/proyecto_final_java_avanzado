fdd4169257ca32b14e8be992e54088be28cc75f0
AlejRes <alejrespal@gmail.com>
2022-10-17 18:44:42 +0200

    project structure

---
+++ src/test/java/com/alejrest/proyectoSesion1/ProyectoSesion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejrest.proyectoSesion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSesion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
